2ed3b9791222ec08a1e9001e77afecf2b3810456
mkristian <m.kristian@web.de>
2010-10-15 09:48:24 +0530

    perstine rails

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '2810def8ee79b6cee999d41f27aff4d3a9f31ae6d018a3032635b75bf24d65e219473b6a5f1931663509a3f051deb6a8771684fa94c57731872d130b5d48b39f'
