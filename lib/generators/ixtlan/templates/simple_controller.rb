3208e2f1a26fc677a2d24bfadbfddf28390232b2
Kristian Meier <m.kristian@web.de>
2011-07-23 00:05:53 +0530

    removed obsolete and confusing source_path

--- lib/generators/ixtlan/templates/simple_controller.rb
+++
@@ -1,4 +0,0 @@
-require 'ixtlan/controllers/<%= plural_file_name %>_controller'
-class <%= controller_class_name %>Controller < ApplicationController
-  include ::Ixtlan::Controllers::<%= controller_class_name %>Controller
-end
