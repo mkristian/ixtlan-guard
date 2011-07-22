cb95bd31e662757ba165e571b03dd004247c4be4
Kristian Meier <m.kristian@web.de>
2011-07-23 02:19:42 +0530

    Revert "removed obsolete and confusing source_path"
    This reverts commit cc8fc46d43fe3c3158fae3dc76f5218099f4a1c5.

--- lib/ixtlan/guard/railtie.rb
+++ lib/ixtlan/guard/railtie.rb
@@ -20,6 +20,16 @@ module Ixtlan
           logger.warn e.message
         end
       end
+      
+      config.generators do
+        require 'rails/generators'
+        require 'rails/generators/rails/controller/controller_generator'
+        require 'rails/generators/erb/scaffold/scaffold_generator'
+        Rails::Generators::ControllerGenerator.hook_for :ixtlan, :type => :boolean, :default => true do |controller|
+          invoke controller, [ class_name, actions ]
+        end
+        Erb::Generators::ScaffoldGenerator.source_paths.insert(0, File.expand_path('../../generators/ixtlan/templates', __FILE__))
+      end
     end
   end
 end
