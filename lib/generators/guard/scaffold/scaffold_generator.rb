6a7616d90bbe7bf73870ab2d6e1d40d1ba79eda1
Kristian Meier <m.kristian@web.de>
2011-09-05 10:37:21 +0530

    get the scaffold woking again

--- lib/generators/guard/scaffold/scaffold_generator.rb
+++ lib/generators/guard/scaffold/scaffold_generator.rb
@@ -3,12 +3,17 @@ module Guard
   class ScaffoldGenerator < Rails::Generators::NamedBase
     include Rails::Generators::ResourceHelpers
 
-    source_root File.expand_path('../templates', __FILE__)
+    source_root File.expand_path('../../templates', __FILE__)
     
 #    check_class_collision :suffix => "Guard"
     
     def create_guard_files
       template 'guard.yml', File.join('app', 'guards', class_path, "#{plural_file_name}_guard.yml")
+    end
+ 
+    #TODO should be coming from the actual generator
+    def actions
+      ['index', 'show', 'new', 'create', 'edit', 'update', 'destroy']
     end
   end
 end
